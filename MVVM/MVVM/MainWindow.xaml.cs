57daaea380005dad37d4a513c834c69a33ea9582
romjomiees <100134014+romjomiees@users.noreply.github.com>
2024-02-06 10:51:56 +0200

    Add files via upload

---
+++ MVVM/MVVM/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace MVVMDemo
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            MVVMDemo.ViewModel.StudentViewModel studentViewModelObject =
+               new MVVMDemo.ViewModel.StudentViewModel();
+            studentViewModelObject.LoadStudents();
+
+            StudentViewControl.DataContext = studentViewModelObject;
+        }
+    }
+}
